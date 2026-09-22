cccbd50f0c7eb525737b965ca6ec0ceb0327269e
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-09-22 09:58:38 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+</script>
+
+<template>
+  <div class="grid min-h-screen gap-x-10 dark:bg-black dark:text-white" :class="$style.wrapper">
+    <Header />
+  </div>
+</template>
+
+<style module>
+.wrapper {
+  grid-template-columns: [full] 1fr [main] minmax(auto, 1200px) [main-end] 1fr [full-end];
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <header class="col-[main]">header</header>
+</template>
